2bcaa2c37c34d2d6ab9dce9f0737575312606d60
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-09-11 18:15:06 +0200

    migration completada

--- database/migrations/2024_09_11_132411_create_activities_table.php
+++ database/migrations/2024_09_11_132411_create_activities_table.php
@@ -14,13 +14,6 @@ return new class extends Migration
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
             $table->enum('type', ['surf', 'windsurf', 'kayak', 'atv', 'hot air balloon']);
-            $table->foreignId('user_id')->constrained(
-                table:'users', indexName:'posts_user_id'
-            )->onDelete('cascade');
-            $table->dateTime('datetime');
-            $table->boolean('paid')->default(false);
-            $table->text('notes');
-            $table->interger('satisfaction')->default(null)->nullable();
             $table->timestamps();
         });
     }
